31993a7900b6ef63bef939bbd19c9d3f5486f263
Wolanicja <kubas1904@gmail.com>
2026-07-21 22:17:30 +0200

    kompost świerzy

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ import sys
 import os 
 import random
 import string
+<<<<<<< Updated upstream
 from cryptography import Fernet
+=======
+from cryptography import fernet
+
+print("""What do you want to do?
+      1.Generate new key.
+      2.Encrypt your message.
+      3.Decrypt a message(Remember to have the same key as the sender of the message)
+      
+      """)
+choice = int(input("Choose a number:"))
+>>>>>>> Stashed changes
